b9cd56d88366fa41f9dea5e388cd731202ded0b1
Rogerio J. Gentil <rogerio_gentil@yahoo.com.br>
2017-11-20 09:51:30 -0200

    Unit test for R-6.10

---
+++ src/test/java/Exercise10Test.java
@@ -0,0 +1,45 @@
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import rogeriogentil.data.structures.chapter06.reinforcement.Exercise10;
+
+/**
+ *
+ * @author Rogerio J. Gentil
+ */
+public class Exercise10Test {
+
+   private Exercise10<Integer> ex10;
+
+   @Before
+   public void beforeEachTest() {
+      ex10 = new Exercise10<>();
+   }
+
+   @Test
+   public void dequeMustBeEmtpyWhenInitialized() {
+      assertThat(ex10.isEmpty(), is(true));
+   }
+
+   @Test
+   public void lastElementMustBeTheTop() {
+      ex10.push(10);
+      ex10.push(20);
+      ex10.push(30);
+
+      assertThat(ex10.top(), is(equalTo(30)));
+   }
+
+   @Test
+   public void mustRemoveTheElementOnTheTop() {
+      ex10.push(10);
+      ex10.push(20);
+      ex10.push(30);
+      
+      ex10.pop();
+
+      assertThat(ex10.top(), is(equalTo(20)));
+   }
+}
