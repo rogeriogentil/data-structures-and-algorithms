b65dc21659e768b0340895c865cbc886049f209d
Rogerio J. Gentil <rogerio_gentil@yahoo.com.br>
2017-11-20 11:07:11 -0200

    Correction of the exercises of the chapter 6's reinforcement
    ... and addition of the question description.

--- src/main/java/rogeriogentil/data/structures/chapter06/reinforcement/Exercise04.java
+++ src/main/java/rogeriogentil/data/structures/chapter06/reinforcement/Exercise04.java
@@ -3,7 +3,10 @@ package rogeriogentil.data.structures.chapter06.reinforcement;
 import rogeriogentil.data.structures.chapter06.Stack;
 
 /**
- *
+ * R-6.4 Implement a method with signature transfer(S, T ) that transfers all elements from stack S onto stack T , so 
+ * that the element that starts at the top of S is the first to be inserted onto T , and the element at the bottom of 
+ * S ends up at the top of T.
+ * 
  * @author Rogerio J. Gentil
  */
 public class Exercise04 {
